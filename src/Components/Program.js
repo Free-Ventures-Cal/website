4a1b1c3118e3453b786b6b1fa3cd62535a3bfa9a
Edward Luna <mister.luna@berkeley.edu>
2021-08-18 15:09:58 -0700

    [feat] work on track record styling top half

--- src/Components/Program.js
+++ src/Components/Program.js
@@ -116,8 +116,8 @@ function Program() {
                         <h2>Our Track Record</h2>
                         <div className="track-record-top">
                             <h3>Lorem Ipsum</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <button>See all our companies</button>
+                            <p className="track-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <button className="tr-button"><p>See all our companies</p></button>
                         </div>
                         <div className="track-record-bottom">
                             <div className="tr-bottom-left">
