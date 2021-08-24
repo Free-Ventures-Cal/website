d418b9f80494b745ab347abc633e18b95e472e43
Edward Luna <mister.luna@berkeley.edu>
2021-08-24 13:05:44 -0700

    [feat] link track record button to portfolio

--- src/Components/Program.js
+++ src/Components/Program.js
@@ -117,7 +117,7 @@ function Program() {
                         <div className="track-record-top tr-container">
                             <h3>Lorem Ipsum</h3>
                             <p className="track-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <button className="tr-button"><p>See all our companies</p></button>
+                            <a href="/portfolio" className="tr-button button"><p>See all our companies</p></a>
                         </div>
                         <div className="track-record-bottom tr-container">
                             <div className="tr-bottom-section">
