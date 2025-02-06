581e1eeba1ae79b7af05161d17d10b158581cc8a
Rohan Taneja <47066511+R-Taneja@users.noreply.github.com>
2025-02-06 12:58:06 -0800

    Change portfolio description

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -92,7 +92,7 @@ class Portfolio extends React.Component {
         <GenericTop
           header={"Portfolio"}
           description={
-            "Our founders have gone on to become Forbes 30 Under 30, build companies that are changing the world, and continue to develop the college startup scene by creating The House. We have helped our 92 alumni teams raise over $170 million from top firms such as Y Combinator, Greylock, KPCB, and Accel."
+            "Our founders have gone on to become Forbes 30 Under 30, build companies that are changing the world, and continue to develop the college startup scene by creating The House. We have helped our 100+ alumni teams raise over $200M from top firms such as Y Combinator, Greylock, KPCB, and Accel."
           }
         />
         <GenericPage>
